c13f9a1faacb61026aeeefdc354b3cbc02e895a8
jenny-huaang <40249176+jenny-huaang@users.noreply.github.com>
2020-09-28 16:44:30 -0700

    Updates

---
+++ precache-manifest.735e03c42d8271da9322b5af18b4cfbc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47f6a25b4c46092b9f598f394f8245ed",
+    "url": "/hyre-starships/index.html"
+  },
+  {
+    "revision": "5d3aac58ac7628ae0187",
+    "url": "/hyre-starships/static/css/main.5963e852.chunk.css"
+  },
+  {
+    "revision": "697d5cff95ef13a05b94",
+    "url": "/hyre-starships/static/js/2.fce9b736.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/hyre-starships/static/js/2.fce9b736.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d3aac58ac7628ae0187",
+    "url": "/hyre-starships/static/js/main.d65b9f86.chunk.js"
+  },
+  {
+    "revision": "e17b7221d046d7120706",
+    "url": "/hyre-starships/static/js/runtime-main.6cf89b49.js"
+  },
+  {
+    "revision": "c43da60a4d03063c95e8f44b52a67cd8",
+    "url": "/hyre-starships/static/media/greg-rakozy-unsplash.c43da60a.jpg"
+  }
+]);
