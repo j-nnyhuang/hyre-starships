33f91fadcb7a2c30cebb6cb48ac99edb8a145232
jenny-huaang <40249176+jenny-huaang@users.noreply.github.com>
2020-09-28 23:41:46 -0700

    Initial look

--- starships-website/src/pages/homePage.jsx
+++ starships-website/src/pages/homePage.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
-import "../assets/css/fonts.scss";
-import backgroundPhoto from "../assets/img/greg-rakozy-unsplash.jpg";
-
+import starships from "../assets/img/starships.png";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../assets/scss/fonts.scss";
+import { Card, Button, Modal, Row } from "react-bootstrap";
 import { useQuery } from "@apollo/react-hooks";
 import { gql } from "apollo-boost";
 
@@ -34,52 +35,83 @@ const GET_STARSHIPS_INFO = gql`
 
 class Cover extends Component {
   render() {
-    const img = {
+    const background = {
       width: "100vw",
       height: "100vh",
-      backgroundImage: `url(${backgroundPhoto})`,
-      backgroundSize: "100%",
-      backgroundPosition: "bottom",
-      zIndex: "-1",
-      position: "absolute",
-      top: 0,
+      backgroundColor: "#000",
+      textAlign: "center",
+      paddingTop: "25vh",
     };
 
     const headingStyle = {
       fontSize: "8vw",
       color: "#fff",
-      textAlign: "center",
+      margin: "0",
+      paddingTop: "15vh",
     };
     return (
       <React.Fragment>
-        <div style={img}></div>
-        <h1 style={headingStyle}>STARSHIPS</h1>
+        <div style={background}>
+          <div style={{ width: "100vw" }}>
+            <img src={starships} style={{ width: "50vw" }} />
+          </div>
+          <Button href="#starships" variant="secondary">
+            browse
+          </Button>
+        </div>
       </React.Fragment>
     );
   }
 }
 
 function Starships() {
+  // Data querying
   const {
     loading: loadingStarships,
     error: errorStarships,
     data: dataStarships,
   } = useQuery(GET_STARSHIPS_INFO);
-
   if (loadingStarships) return <p>Loading...</p>;
   if (errorStarships) return <p>There's an error: {errorStarships.message}</p>;
-  console.dir(dataStarships);
-  const starships = dataStarships.allStarships.map((starships) => (
-    <div>
-      <p>
-        <b>{starships.name}</b>
-      </p>
-    </div>
-  ));
+  console.log(dataStarships);
+  const starships = dataStarships.allStarships.edges.map((starship) => {
+    return (
+      <div key={starship.node.id}>
+        <Card
+          bg="dark"
+          className="mb-2"
+          style={{ width: "15rem", margin: "15px" }}
+        >
+          <Card.Body>
+            <Card.Title style={{ color: "#ffffff" }}>
+              {starship.node.name}
+            </Card.Title>
+
+            <Card.Text style={{ color: "#000" }}>
+              <h5> This starship appears in:</h5>
+              {starship.node.filmConnection.films.map((film) => {
+                return <p style={{ marginBottom: "0" }}>{film.title}</p>;
+              })}
+              <h5 style={{ marginTop: "5px" }}>
+                {" "}
+                This starship is piloted by:
+              </h5>
+              {starship.node.pilotConnection.pilots.map((pilot) => {
+                return <p style={{ marginBottom: "0" }}>{pilot.name}</p>;
+              })}
+            </Card.Text>
+            {/* <Card.Link onClick={handleShow}>More Info</Card.Link> */}
+          </Card.Body>
+        </Card>
+      </div>
+    );
+  });
   return (
     <React.Fragment>
-      <h1>Star Wars Films</h1>
-      {starships}
+      <div style={{ backgroundColor: "#000" }}>
+        <Row style={{ justifyContent: "center" }}>{starships}</Row>
+      </div>
+      ;
     </React.Fragment>
   );
 }
@@ -88,7 +120,9 @@ export default function HomePage() {
   return (
     <React.Fragment>
       <Cover />
-      <Starships />
+      <div id="starships">
+        <Starships />
+      </div>
     </React.Fragment>
   );
 }
